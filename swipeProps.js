843b8fbbf26f41108bf61fc49d4e362ae2f894c1
Alex Lunyov <isuntc@gmail.com>
2019-01-17 21:52:11 +0800

    Implemented swipe gesture. Preventing scroll during drag

--- swipeProps.js
+++ swipeProps.js
@@ -80,53 +80,93 @@ export default function ReactSwipeProps({ children, pos: propsPos, min, max, tra
     }
   }, [dst, propsPos]);
 
-  const handleDragStart = (e) => {
-    const { pageX, pageY } = e.touches ? e.touches[0] : e;
-    const state = { x: pageX, y: pageY, dragging: true, delta: 0 };
-    e.preventDefault();
+  const limitPos = (n) => Math.min(Math.max(Math.round(n), min), max)
 
-    const calcSpeed = (delta) => {
-      const currentPos = pos + state.delta;
-      if (currentPos <= min && delta < 0) {
-        return Math.max(0, 0.3 + state.delta / 2);
-      } else if(currentPos >= max && delta > 0) {
-        return Math.max(0, 0.3 - state.delta / 2);
+  useEffect(() => {
+    const handlerOptions = { passive: false };
+    const handleDragStart = (e) => {
+      const { pageX, pageY } = e.touches ? e.touches[0] : e;
+      const state = { x: pageX, y: pageY, dragging: false, delta: 0 };
+      const startTime = Date.now();
+
+      const calcSpeed = (delta) => {
+        const currentPos = pos + state.delta;
+        if (currentPos <= min && delta < 0) {
+          return Math.max(0, 0.3 + state.delta / 2);
+        } else if(currentPos >= max && delta > 0) {
+          return Math.max(0, 0.3 - state.delta / 2);
+        }
+
+        return 0.8;
       }
 
-      return 0.8;
-    }
+      const removeListeners = () => {
+        document.removeEventListener('touchmove', handleMove, handlerOptions);
+        document.removeEventListener('touchend', handleEnd, handlerOptions);
+        document.removeEventListener('mousemove', handleMove);
+        document.removeEventListener('mouseup', handleEnd);
+      }
 
-    const handleMove = (e) => {
-      if (state.dragging) {
-        e.preventDefault();
+      const handleMove = (e) => {
         const { pageX, pageY } = e.touches ? e.touches[0] : e;
-        const delta = (state.x - pageX) / rect.width;
-        const speed = calcSpeed(delta);
-        state.delta += delta * speed;
-        state.x = pageX;
-        state.y = pageY;
+        const deltaX = (state.x - pageX);
+        const deltaY = (state.y - pageY);
+        if (state.dragging) {
+          e.preventDefault();
+          const deltaPos = deltaX / rect.width;
+          const speed = calcSpeed(deltaPos);
+          state.delta += deltaPos * speed;
+          state.x = pageX;
+          state.y = pageY;
 
-        setPos(pos + state.delta);
-        setDst(pos + state.delta);
+          setPos(pos + state.delta);
+          setDst(pos + state.delta);
+        } else {
+          const niceTouch = e.touches ? !(e.touches.length > 1 || (e.scale && e.scale !== 1)) : true;
+          if (niceTouch && Math.abs(deltaX) > Math.abs(deltaY)) {
+            console.log('DRAG');
+            e.preventDefault();
+            state.dragging = true;
+          } else {
+            removeListeners();
+          }
+        }
+      }
+
+      const handleEnd = () => {
+        const final = limitPos(Math.round(pos + state.delta));
+
+        if (final === pos && Date.now() - startTime < 250 && Math.abs(state.delta * rect.width) > 30) {
+          setDst(limitPos(final + Math.sign(state.delta)));
+        } else {
+          setDst(final);
+        }
+        removeListeners();
+      }
+
+      document.addEventListener('touchmove', handleMove, handlerOptions);
+      document.addEventListener('touchend', handleEnd, handlerOptions);
+      document.addEventListener('mousemove', handleMove);
+      document.addEventListener('mouseup', handleEnd);
+    };
+
+    if (root.current) {
+      root.current.addEventListener('touchstart', handleDragStart, handlerOptions);
+      root.current.addEventListener('touchforcechange', () => undefined, false);
+      root.current.addEventListener('mousedown', handleDragStart);
+
+      return () => {
+        if (root.current) {
+          root.current.removeEventListener('touchstart', handleDragStart, handlerOptions);
+          root.current.removeEventListener('touchforcechange', () => undefined, false);
+          root.current.removeEventListener('mousedown', handleDragStart);
+        }
       }
     }
-
-    const handleEnd = () => {
-      state.dragging = false;
-      setDst(Math.min(Math.max(Math.round(pos + state.delta), min), max));
-      document.removeEventListener('touchmove', handleMove);
-      document.removeEventListener('touchend', handleEnd);
-      document.removeEventListener('mousemove', handleMove);
-      document.removeEventListener('mouseup', handleEnd);
-    }
-    document.addEventListener('touchmove', handleMove);
-    document.addEventListener('touchend', handleEnd);
-    document.addEventListener('mousemove', handleMove);
-    document.addEventListener('mouseup', handleEnd);
-  };
+  });
 
   return (
-    <div className="swipeRoot" ref={root} onMouseDown={handleDragStart} onTouchStart={handleDragStart} {...props}>
+    <div className="swipeRoot" ref={root} {...props}>
       {children(pos)}
     </div>
   );
